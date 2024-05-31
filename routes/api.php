d63c4b175f5dbb7d15abbe530ae67f3c60051553
JohnRexBanal <banaljohnrex99@gmail.com>
2024-05-31 10:30:59 +0800

    Updates - added new model for Product - modified and use a HasAPITokens on Users Model - modified routes api for login,users, and products

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// User routes
+Route::middleware('auth:sanctum')->prefix('users')->group(function () {
+    Route::get('/users', [AuthController::class, 'index']);
+    Route::get('/view/{id}', [AuthController::class, 'show']);
+    Route::put('/update/{id}', [AuthController::class, 'update']);
+    Route::delete('/delete/{id}', [AuthController::class, 'destroy']);
+    Route::get('/count', [AuthController::class, 'count']);
+});
+
+// Product routes
+Route::middleware('auth:sanctum')->prefix('products')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/create', [ProductController::class, 'store']);
+    Route::get('/edit/{id}', [ProductController::class, 'edit']);
+    Route::put('/update/{id}', [ProductController::class, 'update']);
+    Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
+    Route::get('/view/{id}', [ProductController::class, 'show']);
+    Route::get(('/count'), [ProductController::class, 'count']);
+});
