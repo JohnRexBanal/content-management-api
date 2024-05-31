75b23c0d96827677c8345e39acda9c01a1582c11
JohnRexBanal <banaljohnrex99@gmail.com>
2024-05-31 11:57:36 +0800

    Modified the AutController -added admin validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,14 +15,15 @@ class AuthController extends Controller
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
+
         // Attempting authentication with provided email and password
         if (auth()->attempt($request->only('email', 'password'))) {
             $user = auth()->user();
-            $token = $user->createToken('token-name')->plainTextToken;
+            
+            $token = $user->createToken($user->is_admin ? 'admin-token' : 'user-token')->plainTextToken;
 
-            return response()->json(['token' => $token], 201);
+            return response()->json(['token' => $token, 'user' => $user], 201);
         }
-
         // If authentication fails, redirect back with input and a warning message
         return response()->json(['message' => 'Invalid username or password'], 401);
     }
@@ -53,5 +54,64 @@ class AuthController extends Controller
         
 
         return response()->json(['message' => 'User registered successfully'], 201);
+    }
+
+    public function index() {
+
+        $user = User::all();
+
+        return response()->json($user, 200);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+    public function update(Request $request, $id) {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'email' => ['sometimes', 'required', 'email', Rule::unique('users')->ignore($user->id)],
+            'password' => 'sometimes|required|string|min:8|confirmed|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+        ]);
+
+        if (isset($validated['password'])) {
+            $validated['password'] = bcrypt($validated['password']);
+        }
+
+        $user->update($validated);
+
+        return response()->json(['message' => 'User updated successfully']);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+
+    public function count()
+    {
+        $count = User::count();
+        return response()->json(['count' => $count], 200);
     }
 }
